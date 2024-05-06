67bc92a4709a732f16d580c4ea3a59cf79c72a52
guillaumelfv <glefevre.inbox@gmail.com>
2024-05-06 19:26:36 +0700

    add windows control to wezterm conf

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,6 +1,9 @@
 -- Pull in the wezterm API
 local wezterm = require 'wezterm'
 
+-- wezterm.mux module manages the multiplexer layer (panes, tabs, windows, and workspaces)
+local mux = wezterm.mux
+
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
@@ -12,7 +15,22 @@ config.color_scheme = 'Default (dark) (terminal.sexy)'
 config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
 
-config.window_background_opacity = 0.8 -- personal recommended value
+config.window_background_opacity = 0.9
+
+config.inactive_pane_hsb = {
+    saturation = 0.8,
+    brightness = 0.7,
+}
+
+-- Mazimize window  on startup
+wezterm.on('gui-startup', function(window)
+    local tab, pane, window = mux.spawn_window(cmd or {})
+    local gui_window = window:gui_window();
+    gui_window:perform_action(wezterm.action.ToggleFullScreen, pane)
+end)
+
+-- Disable title bar and enable resizable border
+config.window_decorations = "RESIZE"
 
 -- and finally, return the configuration to wezterm
 return config
